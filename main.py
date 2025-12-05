50abf6494873f0c22e4f4fb7f73d09a1437840fd
prdavis82 <paul.r.davis1983@gmail.com>
2025-12-04 19:55:49 -0600

    bookbot report

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ def frankenstein_fetch():
     text = reader("books/frankenstein.txt")
     counts = character_count(text)
     sorted_chars = report(counts)
-    print(f"Found {counter(text)} total words")
-    print(sorted_chars)
+    print(f"""
+============ BOOKBOT ============
+Analyzing book found at books/frankenstine.txt...
+----------- Word Count ----------
+Found {counter(text)} total words
+--------- Character Count -------
+    """)
+    for char_dict in sorted_chars:
+        print(f"{char_dict["char"]}: {char_dict["num"]}")
+    print("============= END ===============")
 
 frankenstein_fetch()
--- stats.py
+++ stats.py
@@ -2,22 +2,22 @@ def counter(text):
         return len(text.split())
 
 def character_count(text):
-	lower_case = str.lower(text)
-	letter_count = {}
-	for letter in lower_case:
-		if letter in letter_count:
-			letter_count[letter] += 1
-		else:
-			letter_count[letter] = 1
-	return letter_count
+    lower_case = str.lower(text)
+    letter_count = {}
+    for letter in lower_case:
+        if letter in letter_count:
+            letter_count[letter] += 1
+        else:
+            letter_count[letter] = 1
+    return letter_count
 
 def sort_on(item):
     return item["num"]
 
 def report(letter_count):
-	results = []
-	for ch, cnt in letter_count.items():
-		if ch.isalpha():
-			results.append({"char":ch, "num":cnt})
-	results.sort(reverse=True, key=sort_on)
-	return results
+    results = []
+    for ch, cnt in letter_count.items():
+        if ch.isalpha():
+            results.append({"char": ch, "num": cnt})
+    results.sort(reverse=True, key=sort_on)
+    return results
